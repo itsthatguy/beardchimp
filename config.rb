431eaf1616fe29261bd9d97d98dc35b6fd49fe29
Lake & Kevin <pair@beardson.ly>
2012-03-16 00:49:09 -0400

    Add deploy script and get things working

--- config.rb
+++ config.rb
@@ -33,7 +33,7 @@
 # Per-page layout changes:
 # 
 # With no layout
-# page "/path/to/file.html", :layout => false
+#page "/index.slim", :layout => :layout
 # 
 # With alternative layout
 # page "/path/to/file.html", :layout => :otherlayout
